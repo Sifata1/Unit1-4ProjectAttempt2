8517a2ba3e63ec9d335f3cef25b07ddd80d38146
BT_4N2_01 <stuff.com>
2022-11-10 12:26:54 -0500

    started

--- src/StoreSimulator.java
+++ src/StoreSimulator.java
@@ -19,10 +19,17 @@ public class StoreSimulator {
         pear = pears;
     }
 
+    public int Apples(int apples) {
+        return apples;
+    }
     public String apples(int apples) {
         double cost = (apples * 0.99);
-        return "You have bought " + apples + "apples with " + cost;
+        return "You have bought " + apples + "apples with " + (cost*apples);
     }
 
+    public String bananas(int bananas) {
+        double cost = (bananas * 0.75);
+        return "You have bought " + bananas + "apples with " + (cost*bananas);
+    }
 
 }
--- src/StoreSimulatorRunner.java
+++ src/StoreSimulatorRunner.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class StoreSimulatorRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Welcome to Sifat's Marketplace! We sell apples, bananas, watermelons, peaches, oranges, pomegranates, and pears.");
+        System.out.println("Welcome to Walmart! We now only sell apples, bananas, watermelons, peaches, oranges, pomegranates, and pears.");
         System.out.println("Are you ready to begin!");
         System.out.println();
 
@@ -36,6 +36,13 @@ public class StoreSimulatorRunner {
 
         StoreSimulator trip1 = new StoreSimulator(a,b,w,p,o,po,pe);
 
+        System.out.println("To confirm, you bought: ");
+        System.out.println(trip1.Apples(a) + " apples");
 
+        System.out.println("-------------------------");
+        System.out.println("Walmart");
+        System.out.println("29 Fort Greene Pl, Brooklyn, NY 11217");
+        System.out.println();
+        System.out.println(a +" apples - " + "$" + (0.99*a));
     }
 }
